d8d6fb6de3e001197403555a05959b6deac07451
Jeroen Overschie <jeroenoverschie@gmail.com>
2019-06-26 21:39:52 +0200

    Read in other image sets

--- road-marker-classification.py
+++ road-marker-classification.py
@@ -4,7 +4,23 @@
 
 #%%
 from skimage.io import imread_collection
-ic = imread_collection('./data/groundtruth/image?.png', False)
-for im in ic:
+
+# Ground truth
+gt = imread_collection('./data/groundtruth/image?.png', False)
+# Supervised
+sv = imread_collection('./data/supervised/image?.png', False)
+# Unsupervised
+usv = imread_collection('./data/unsupervised/output/output_image?.png', False)
+
+print('Grouth truth image shapes:')
+for im in gt:
+    print(im.shape)
+print('Supervised approach image shapes:')
+for im in sv:
+    print(im.shape)
+print('Un-supervised approach image shapes:')
+for im in usv:
     print(im.shape)
 print('End of program.')
+
+#%%
