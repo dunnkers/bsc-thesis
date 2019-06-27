4486d393f7de114077e256162043096325b86071
dunnkers <jeroenoverschie@gmail.com>
2019-06-27 10:32:39 +0200

    Plot of every time its first image ✌🏼

--- road-marker-classification.py
+++ road-marker-classification.py
@@ -12,15 +12,45 @@ sv = imread_collection('./data/supervised/image?.png', False)
 # Unsupervised
 usv = imread_collection('./data/unsupervised/output/output_image?.png', False)
 
-print('Grouth truth image shapes:')
-for im in gt:
-    print(im.shape)
-print('Supervised approach image shapes:')
-for im in sv:
-    print(im.shape)
-print('Un-supervised approach image shapes:')
-for im in usv:
-    print(im.shape)
-print('End of program.')
+print('gt size: {}, sv size: {}, usv size: {}'.format(
+        gt.data.size, sv.data.size, usv.data.size
+    ))
+
+#%% [markdown]
+# Plot an image of each data type.
+
+#%%
+from matplotlib import pyplot
+from skimage.io import imshow
+
+pyplot.subplot(2, 3, 1).set_title("Ground truth")
+imshow(gt[0])
+ax_hist = pyplot.subplot(2, 3, 4)
+ax_hist.hist(gt[0].ravel(), bins=256)
+
+pyplot.subplot(2, 3, 2).set_title("Supervised")
+imshow(sv[0])
+ax_hist = pyplot.subplot(2, 3, 5)
+ax_hist.hist(sv[0].ravel(), bins=256)
+
+pyplot.subplot(2, 3, 3).set_title("Unsupervised")
+imshow(usv[0])
+ax_hist = pyplot.subplot(2, 3, 6)
+ax_hist.hist(usv[0].ravel(), bins=256)
+
+pyplot.show()
+
+print('Ground truth:\tshape {}\tmin,max({}, {})'.format(
+        gt[0].shape, gt[0].min(), gt[0].max()
+    ))
+print('Supervised:\tshape {}\tmin,max({}, {})'.format(
+        sv[0].shape, sv[0].min(), sv[0].max()
+    ))
+print('Unsupervised:\tshape {}\tmin,max({}, {})'.format(
+        usv[0].shape, usv[0].min(), usv[0].max()
+    ))
+
+#%% [markdown]
+# Transform image data into feature vectors.
 
 #%%
