1a43ac4720140eb46bbd7cbc4ae14a1a286b3920
dunnkers <jeroenoverschie@gmail.com>
2019-06-30 17:14:52 +0200

    Perform thresholding

--- road-marker-classification.py
+++ road-marker-classification.py
@@ -72,9 +72,13 @@ pyplot.show()
 
 #%%
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.base import BaseEstimator, TransformerMixin
+
 from skimage.color import rgb2gray
 from skimage.transform import resize
+from skimage.exposure import rescale_intensity
+from skimage.filters import threshold_otsu
 
 class ResizeTransform(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -85,6 +89,26 @@ class ResizeTransform(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y=None):
         return np.array([resize(img, SIZE_NORMAL_SHAPE) for img in X])
+
+class StretchTransform(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return np.array([rescale_intensity(img) for img in X])
+
+class ThresholdingTransform(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return np.array([img > threshold_otsu(img) for img in X])
 
 class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -97,16 +121,23 @@ class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
         return [rgb2gray(img) for img in X]
 
 resizer = ResizeTransform()
+stretcher = StretchTransform()
 grayify = RGB2GrayTransformer()
+binarizer = LabelBinarizer()
+thresholder = ThresholdingTransform()
 
 ## Training
 # Transform Ground Truth
-gt_transformed = grayify.fit_transform(gt)
-gt_resized = resizer.fit_transform(gt_transformed)
+gt_grayed = grayify.fit_transform(gt)
+gt_resized = resizer.fit_transform(gt_grayed)
+gt_prepared = thresholder.fit_transform(gt_resized)
 # Transform supervised
 sv_resized = resizer.fit_transform(sv)
+sv_prepared = thresholder.fit_transform(sv_resized)
 # Transform unsupervised
 usv_resized = resizer.fit_transform(usv)
+usv_stretched = stretcher.fit_transform(usv_resized)
+usv_prepared = thresholder.fit_transform(usv_stretched)
 
 ## Testing
 # Transform Ground Truth
@@ -124,29 +155,23 @@ usv_resized_test = resizer.transform(usv_test)
 #%%
 print('Prepared training image data for index = 0:')
 
-print('Ground truth:\tshape {}'.format(
-        gt_resized[0].shape
+pyplot.subplot(1, 3, 1).set_title("Ground truth")
+imshow(gt_prepared[0])
+print('Ground truth:\tshape {}\tmin,max({}, {})'.format(
+        gt_prepared[0].shape, gt_prepared[0].min(), gt_prepared[0].max()
     ))
-pyplot.subplot(2, 3, 1).set_title("Ground truth")
-imshow(gt_resized[0])
-ax_hist = pyplot.subplot(2, 3, 4)
-ax_hist.hist(gt_resized[0].ravel(), bins=32)
 
-print('Supervised:\tshape {}'.format(
-        sv_resized[0].shape
+pyplot.subplot(1, 3, 2).set_title("Supervised")
+imshow(sv_prepared[0])
+print('Supervised:\tshape {}\tmin,max({}, {})'.format(
+        sv_prepared[0].shape, sv_prepared[0].min(), sv_prepared[0].max()
     ))
-pyplot.subplot(2, 3, 2).set_title("Supervised")
-imshow(sv_resized[0])
-ax_hist = pyplot.subplot(2, 3, 5)
-ax_hist.hist(sv_resized[0].ravel(), bins=256)
 
-print('Unsupervised:\tshape {}'.format(
-        usv_resized[0].shape
+pyplot.subplot(1, 3, 3).set_title("Unsupervised")
+imshow(usv_prepared[0])
+print('Unsupervised:\tshape {}\tmin,max({}, {})'.format(
+        usv_prepared[0].shape, usv_prepared[0].min(), usv_prepared[0].max()
     ))
-pyplot.subplot(2, 3, 3).set_title("Unsupervised")
-imshow(usv_resized[0])
-ax_hist = pyplot.subplot(2, 3, 6)
-ax_hist.hist(usv_resized[0].ravel(), bins=32)
 
 pyplot.show()
 
@@ -156,10 +181,11 @@ pyplot.show()
 
 #%%
 # @FIXME should probably be a sklearn transformation of sorts.
-""" Combine 2D image pixel values into a tupled flat array. """
+# -> sklearn.preprocessing.PolynomialFeatures perhaps
+""" Combine 2D image pixel values, by addition. Return flat (feature) array. """
 def combineImage(imgA, imgB):
     return np.array([
-        (a, b) for a, b in zip(imgA.flatten(), imgB.flatten())
+        a + b for a, b in zip(imgA.flatten(), imgB.flatten())
     ])
 
 def combineImageSet(setA, setB):
@@ -173,7 +199,7 @@ def flattenImageSet(imgSet):
     ])
 
 ## Training
-X_train = combineImageSet(sv_resized, usv_resized)
+X_train = combineImageSet(sv_resized, usv_prepared)
 y_train = flattenImageSet(gt_resized)
 
 ## Testing
@@ -190,8 +216,20 @@ print('y_test shape:', y_test.shape)
 
 #%%
 from sklearn.linear_model import SGDClassifier
-sgd_clf = SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
-sgd_clf.fit(X_train, y_train)
 
-y_pred = sgd_clf.predict(X_test)
+# sgd_clf = SGDClassifier(random_state=42, max_iter=1000, tol=1e-3)
+# sgd_clf.fit(X_train, y_train)
+# y_pred = sgd_clf.predict(X_test)
+
+from sklearn.linear_model import SGDRegressor
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.svm import SVC
+
+clf = SVC(gamma='auto')
+sgd_regr = SGDRegressor()
+multout_clf = MultiOutputClassifier(sgd_regr)
+multout_clf.fit(X_train, y_train)
+y_pred = multout_clf.predict(X_test)
+y_pred
+
 print('End of program stub.')
