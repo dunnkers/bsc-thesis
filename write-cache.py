fb7fa8be85b2b17dbbe05f8f0367920f21c9e630
Jeroen Overschie <jeroenoverschie@gmail.com>
2019-07-14 15:19:14 +0200

    Cache all collections! 😍

--- write-cache.py
+++ write-cache.py
@@ -5,56 +5,41 @@ print('GT_GLOB =', const.GT_GLOB)
 print('SV_GLOB =', const.SV_GLOB)
 print('USV_GLOB =', const.USV_GLOB)
 
-#%% [markdown]
-# Setup transformers.
-
-#%%
-# from skimage.transform import resize
-# from sklearn.preprocessing import FunctionTransformer
-
-# def resize_image(im, shape):
-#     resize(im, shape, mode='reflect', anti_aliasing=True)
-
-# resizer = FunctionTransformer(resize_image)
-
 #%%
 from os.path import exists, dirname
 from skimage.io import imsave
 from skimage.transform import resize
-# from os.path import splitext, basename, exists
+from skimage.exposure import rescale_intensity
 from os import makedirs
-
-# def constructNewPath(path, new_folder, suffix = ''):
-#     if not exists(new_folder):
-#         makedirs(new_folder)
-
-#     file, ext = splitext(path)
-#     return '{}/{}{}{}'.format(
-#         new_folder, basename(file), suffix, ext
-#     )
+from tqdm import tqdm
 
 def cache_image(im, path, shape):
     """
     Saves a cache version of the image if it does not exist.
 
     Parameters:
-    im ().
+    im (numpy.ndarray) Image to resize and cache.
+    path (string) Path to save cached image to.
+    shape (tuple) Shape to resize image to.
     """
     dirpath = dirname(path)
     if not exists(dirpath):
         makedirs(dirpath)
     
-    imsave(path, resize(im, shape, mode='reflect', anti_aliasing=True),
+    # rescale,.. or,.. 
+    # import warnings
+
+    # with warnings.catch_warnings():
+    #     warnings.simplefilter("ignore")
+
+    imsave(path, resize(rescale_intensity(im), shape, mode='reflect', anti_aliasing=True),
         check_contrast=False)
     pass
 
 def get_impath_cached(impath, cachepath):
     return impath.replace(const.DATA_PATH, cachepath, 1)
 
-# def has_cache(impath, cachepath):
-#     return exists(impath_cached(impath, cachepath))
-
-def cache_collection(ic):
+def cache_collection(ic, desc='Caching image collection'):
     """
     Checks for every image in this collection whether it is
     cached. In case no, it caches a resized image.
@@ -62,7 +47,7 @@ def cache_collection(ic):
     Parameters:
     ic (ImageCollection): The image collection to write cache for.
     """
-    for idx, impath in enumerate(ic.files):
+    for idx, impath in enumerate(tqdm(ic.files, desc=desc)):
         for cachepath, shape in const.CACHES:
             impath_cached = get_impath_cached(impath, cachepath)
 
@@ -71,7 +56,14 @@ def cache_collection(ic):
 
 #%%
 from skimage.io import imread_collection
+
 images = imread_collection(const.GT_GLOB)
-cache_collection(images)
+cache_collection(images, desc='Caching groundtruth')
+
+images = imread_collection(const.SV_GLOB)
+cache_collection(images, desc='Caching supervised')
+
+images = imread_collection(const.USV_GLOB)
+cache_collection(images, desc='Caching unsupervised')
 
 print('end')
