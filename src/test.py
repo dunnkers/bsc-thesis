13b76b640b63b6fec90c286e27250ce6b31a6468
Jeroen Overschie <jeroenoverschie@gmail.com>
2019-07-18 17:52:14 +0200

    Test using k-folds!! (really) bad results for now though...

--- src/test.py
+++ src/test.py
@@ -1,50 +1,52 @@
 #%%
 import pickle
-import constants as const
-from skimage.io import imread_collection
-from sklearn.metrics import accuracy_score
+from os.path import join
+
 import numpy as np
-from tqdm.auto import tqdm
-from sklearn.preprocessing import FunctionTransformer
-
-gt  = imread_collection('./cache_100x200/groundtruth/image1.png')
-sv  = imread_collection('./cache_100x200/supervised/image1.png')
-usv = imread_collection('./cache_100x200/unsupervised/output/output_image1.png')
-
-picklepath = '{}_n={}_SVM.pickle'.format(const.CACHE.path, 1000)
-with open(picklepath, 'rb') as handle:
-    model = pickle.load(handle)
-
-print('model', model)
-
-# Transform
-def im2vec(im):
-    """ Flatten 2D image to a 1D vector. Note that `ravel` creates a view,
-    possibly distorting the original array. """
-    return np.array(im).ravel()
-
-def ic2vecs(ic):
-    """ Maps images in collection to a 1D vector. """
-    return [im2vec(im) for im in tqdm(ic, desc='Vectorizing')]
-
-# Vectorize
-vectorize = FunctionTransformer(ic2vecs, validate=False)
-gt_vec  = vectorize.fit_transform(gt)  # replace because using `ravel`
-sv_vec  = vectorize.fit_transform(sv)  # replace because using `ravel`
-usv_vec = vectorize.fit_transform(usv) # replace because using `ravel`
-# Stack
-X = np.stack((np.hstack(sv_vec), np.hstack(usv_vec)), axis=-1)
-
-# Predict
-print('Predicting')
-predictions = model.predict(X)
-
-#%%
-acc_score = accuracy_score(gt_vec[0], predictions)
-print('acc_score = {}'.format(acc_score))
-
 from matplotlib import pyplot
-from skimage.io import imshow
-im = np.reshape(predictions, (200, 100))
-imshow(im)
-pyplot.show()
+from skimage.io import imread_collection, imsave, imshow
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import FunctionTransformer
+from tqdm.auto import tqdm
+
+from constants import CACHES, PICKLEFILE_PREPARED
+
+
+def test_fold(fold):
+    _, _, X_test, y_test = fold['data']
+    clf = fold['clf']
+    size = fold['size']
+    # cachepath = #fold['cachepath']
+    shape = (200, 100) #fold['shape']
+    
+    # Predict
+    print('Predicting')
+    X_test_split = np.split(X_test, size) # only test one image for now.
+    predictions = clf.predict(X_test_split[0]) # ⚠️ Only start using terminal.
+
+    # Accuracy score
+    print('Computing accuracy')
+    y_test_split = np.split(y_test, size)
+    acc_score = accuracy_score(y_test_split[0], predictions)
+    print('acc_score = {}'.format(acc_score))
+
+    # Reconstruction
+    im = np.reshape(predictions, shape)
+    # imsave('duder.png', im, check_contrast=False)
+    imshow(im)
+    pyplot.show()
+
+def test_cache(cache):
+    picklepath = join(cache.path, PICKLEFILE_PREPARED)
+    with open(picklepath, 'rb') as handle:
+        folded_dataset = pickle.load(handle)
+        n_splits = folded_dataset['n_splits']
+        
+        # Test every fold
+        for i, fold in enumerate(folded_dataset['folds']):
+            print('[{}/{}] Testing fold...'.format(i + 1, n_splits))
+            test_fold(fold)
+
+
+test_cache(CACHES[0])
+print('Finished testing.')
